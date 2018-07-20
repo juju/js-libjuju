6a8ed4b86d1c96ba603689c8fea98a805a815435
Jeff Pihach <jeff.pihach@canonical.com>
2018-07-20 15:57:12 -0600

    Expand the client tests to test for both callback and promise based flows.

--- api/test-client.js
+++ api/test-client.js
@@ -37,7 +37,7 @@ tap.test('connect', t => {
     return prom;
   });
 
-  t.test('returns promise, handles failure to connect', t => {
+  t.test('handles failure to connect via promise', t => {
     const reject = t.rejects(jujulib.connect('wss://1.2.3.4', options), null, 'cannot connect WebSocket: nope');
     ws.close('nope');
     return reject;
@@ -53,20 +53,24 @@ tap.test('connect', t => {
     ws.close('bad wolf');
   });
 
+  function validateLoginFailure(t, error, conn) {
+    helpers.requestEqual(t, ws.lastRequest, {
+      type: 'Admin',
+      request: 'Login',
+      params: {'auth-tag': 'who', credentials: 'secret', macaroons: []},
+      version: 3
+    });
+    t.equal(error, 'bad wolf');
+    t.equal(conn, null);
+    t.end();
+  }
+
   t.test('login failure', t => {
     jujulib.connect('wss://1.2.3.4', options, (err, juju) => {
       t.equal(err, null);
       t.notEqual(juju, null);
       juju.login({user: 'who', password: 'secret'}, (err, conn) => {
-        helpers.requestEqual(t, ws.lastRequest, {
-          type: 'Admin',
-          request: 'Login',
-          params: {'auth-tag': 'who', credentials: 'secret', macaroons: []},
-          version: 3
-        });
-        t.equal(err, 'bad wolf');
-        t.equal(conn, null);
-        t.end();
+        validateLoginFailure(t, err, conn);
       });
       // Reply to the login request.
       ws.reply({error: 'bad wolf'});
@@ -75,20 +79,36 @@ tap.test('connect', t => {
     ws.open();
   });
 
+  t.test('login failure via promise', t => {
+    jujulib.connect('wss://1.2.3.4', options).then(juju => {
+      juju.login({user: 'who', password: 'secret'})
+        .then(conn => t.fail)
+        .catch(error => {
+          validateLoginFailure(t, error, null);
+        });
+      ws.reply({error: 'bad wolf'});
+    });
+    ws.open();
+  });
+
+  function validateRedirectionLoginFailure(t, error, conn) {
+    helpers.requestEqual(t, ws.lastRequest, {
+      type: 'Admin',
+      request: 'RedirectInfo',
+      params: {},
+      version: 3
+    });
+    t.equal(error, 'bad wolf');
+    t.equal(conn, null);
+    t.end();
+  }
+
   t.test('login redirection error failure', t => {
     jujulib.connect('wss://1.2.3.4', options, (err, juju) => {
       t.equal(err, null);
       t.notEqual(juju, null);
       juju.login({}, (err, conn) => {
-        helpers.requestEqual(t, ws.lastRequest, {
-          type: 'Admin',
-          request: 'RedirectInfo',
-          params: {},
-          version: 3
-        });
-        t.equal(err, 'bad wolf');
-        t.equal(conn, null);
-        t.end();
+        validateRedirectionLoginFailure(t, err, conn);
       });
       // Reply to the login request.
       ws.reply({error: 'redirection required'});
@@ -99,42 +119,61 @@ tap.test('connect', t => {
     ws.open();
   });
 
+  t.test('login redirection error failure via promise', t => {
+    jujulib.connect('wss://1.2.3.4', options).then(juju => {
+      juju.login({})
+        .then(conn => t.fail)
+        .catch(error => {
+          validateRedirectionLoginFailure(t, error, null);
+        });
+        // Reply to the login request.
+        ws.reply({error: 'redirection required'});
+        // Reply to the redirectInfo request.
+        ws.reply({error: 'bad wolf'});
+    });
+    ws.open();
+  });
+
+  function validateRedirectionLoginSuccess(t, juju, error, conn) {
+    helpers.requestEqual(t, ws.lastRequest, {
+      type: 'Admin',
+      request: 'RedirectInfo',
+      params: {},
+      version: 3
+    });
+    t.equal(juju.isRedirectionError(error), true);
+    t.equal(error.caCert, 'mycert');
+    t.equal(error.servers.length, 2);
+    t.equal(error.servers[0].value, '1.2.3.4');
+    t.equal(error.servers[0].port, 17070);
+    t.equal(error.servers[0].type, 'ipv4');
+    t.equal(error.servers[0].scope, 'global');
+    t.equal(
+      error.servers[0].url('srv1-uuid'),
+      'wss://1.2.3.4:17070/model/srv1-uuid/api');
+    t.equal(
+      error.servers[0].url('wss://4.3.2.1:443/model/uuid-in-url/api'),
+      'wss://1.2.3.4:17070/model/uuid-in-url/api');
+    t.equal(error.servers[1].value, 'example.com');
+    t.equal(error.servers[1].port, 443);
+    t.equal(error.servers[1].type, 'hostname');
+    t.equal(error.servers[1].scope, 'global');
+    t.equal(
+      error.servers[1].url('ws://4.3.2.1:443/model/another/api'),
+      'wss://example.com:443/model/another/api');
+    t.equal(
+      error.servers[1].url('srv2-uuid'),
+      'wss://example.com:443/model/srv2-uuid/api');
+    t.equal(conn, null);
+    t.end();
+  }
+
   t.test('login redirection error success', t => {
     jujulib.connect('wss://1.2.3.4', options, (err, juju) => {
       t.equal(err, null);
       t.notEqual(juju, null);
       juju.login({}, (err, conn) => {
-        helpers.requestEqual(t, ws.lastRequest, {
-          type: 'Admin',
-          request: 'RedirectInfo',
-          params: {},
-          version: 3
-        });
-        t.equal(juju.isRedirectionError(err), true);
-        t.equal(err.caCert, 'mycert');
-        t.equal(err.servers.length, 2);
-        t.equal(err.servers[0].value, '1.2.3.4');
-        t.equal(err.servers[0].port, 17070);
-        t.equal(err.servers[0].type, 'ipv4');
-        t.equal(err.servers[0].scope, 'global');
-        t.equal(
-          err.servers[0].url('srv1-uuid'),
-          'wss://1.2.3.4:17070/model/srv1-uuid/api');
-        t.equal(
-          err.servers[0].url('wss://4.3.2.1:443/model/uuid-in-url/api'),
-          'wss://1.2.3.4:17070/model/uuid-in-url/api');
-        t.equal(err.servers[1].value, 'example.com');
-        t.equal(err.servers[1].port, 443);
-        t.equal(err.servers[1].type, 'hostname');
-        t.equal(err.servers[1].scope, 'global');
-        t.equal(
-          err.servers[1].url('ws://4.3.2.1:443/model/another/api'),
-          'wss://example.com:443/model/another/api');
-        t.equal(
-          err.servers[1].url('srv2-uuid'),
-          'wss://example.com:443/model/srv2-uuid/api');
-        t.equal(conn, null);
-        t.end();
+        validateRedirectionLoginSuccess(t, juju, err, conn);
       });
       // Reply to the login request.
       ws.reply({error: 'redirection required'});
@@ -158,22 +197,54 @@ tap.test('connect', t => {
     ws.open();
   });
 
+  t.test('login redirection error success via promises', t => {
+    jujulib.connect('wss://1.2.3.4', options).then(juju => {
+      juju.login({})
+        .then(t.fail)
+        .catch(error => {
+          validateRedirectionLoginSuccess(t, juju, error, null);
+        });
+        // Reply to the login request.
+        ws.reply({error: 'redirection required'});
+        // Reply to the redirectInfo request.
+        ws.reply({response: {
+          'ca-cert': 'mycert',
+          'servers': [[{
+            value: '1.2.3.4',
+            port: 17070,
+            type: 'ipv4',
+            scope: 'global'
+          }, {
+            value: 'example.com',
+            port: 443,
+            type: 'hostname',
+            scope: 'global'
+          }]]
+        }});
+    });
+    ws.open();
+  });
+
+  function validateLoginDischargeRequiredNoBakery(t, error, conn) {
+    helpers.requestEqual(t, ws.lastRequest, {
+      type: 'Admin',
+      request: 'Login',
+      params: {macaroons: []},
+      version: 3
+    });
+    t.equal(
+      error,
+      'macaroon discharge is required but no bakery instance provided');
+    t.equal(conn, null);
+    t.end();
+  }
+
   t.test('login discharge required no bakery', t => {
     jujulib.connect('wss://1.2.3.4', options, (err, juju) => {
       t.equal(err, null);
       t.notEqual(juju, null);
       juju.login({}, (err, conn) => {
-        helpers.requestEqual(t, ws.lastRequest, {
-          type: 'Admin',
-          request: 'Login',
-          params: {macaroons: []},
-          version: 3
-        });
-        t.equal(
-          err,
-          'macaroon discharge is required but no bakery instance provided');
-        t.equal(conn, null);
-        t.end();
+        validateLoginDischargeRequiredNoBakery(t, err, conn);
       });
       // Reply to the login request with a discharge required response.
       ws.reply({response: {'discharge-required': 'macaroon'}});
@@ -181,6 +252,31 @@ tap.test('connect', t => {
     // Open the WebSocket connection.
     ws.open();
   });
+
+  t.test('login discharge required no bakery via promises', t => {
+    jujulib.connect('wss://1.2.3.4', options).then(juju => {
+      juju.login({})
+        .then(t.fail)
+        .catch(error => {
+          validateLoginDischargeRequiredNoBakery(t, error, null);
+        });
+        // Reply to the login request with a discharge required response.
+        ws.reply({response: {'discharge-required': 'macaroon'}});
+    });
+    ws.open();
+  });
+
+  function validateLoginDischargeRequiredFailure(t, error, conn) {
+    helpers.requestEqual(t, ws.lastRequest, {
+      type: 'Admin',
+      request: 'Login',
+      params: {macaroons: [['m']]},
+      version: 3
+    });
+    t.equal(error, 'macaroon discharge failed: bad wolf');
+    t.equal(conn, null);
+    t.end();
+  }
 
   t.test('login discharge required failure', t => {
     const options = {
@@ -193,15 +289,7 @@ tap.test('connect', t => {
       t.equal(err, null);
       t.notEqual(juju, null);
       juju.login({macaroons: ['m']}, (err, conn) => {
-        helpers.requestEqual(t, ws.lastRequest, {
-          type: 'Admin',
-          request: 'Login',
-          params: {macaroons: [['m']]},
-          version: 3
-        });
-        t.equal(err, 'macaroon discharge failed: bad wolf');
-        t.equal(conn, null);
-        t.end();
+        validateLoginDischargeRequiredFailure(t, err, conn);
       });
       // Reply to the login request with a discharge required response.
       ws.reply({response: {'discharge-required': 'macaroon'}});
@@ -209,6 +297,37 @@ tap.test('connect', t => {
     // Open the WebSocket connection.
     ws.open();
   });
+
+  t.test('login discharge required failure via promises', t => {
+    const options = {
+      bakery: helpers.makeBakery(false),
+      wsclass: helpers.makeWSClass(instance => {
+        ws = instance;
+      })
+    };
+    jujulib.connect('wss://1.2.3.4', options).then(juju => {
+      juju.login({macaroons: ['m']})
+        .then(t.fail)
+        .catch(error => {
+          validateLoginDischargeRequiredFailure(t, error, null);
+        });
+        // Reply to the login request with a discharge required response.
+        ws.reply({response: {'discharge-required': 'macaroon'}});
+    });
+    ws.open();
+  });
+
+  function validateLoginDischargeRequiredSuccess(t, error, conn) {
+    helpers.requestEqual(t, ws.lastRequest, {
+      type: 'Admin',
+      request: 'Login',
+      params: {macaroons: [['m1', 'm2']]},
+      version: 3
+    });
+    t.equal(error, null);
+    t.notEqual(conn, null);
+    t.end();
+  }
 
   t.test('login discharge required success', t => {
     const options = {
@@ -221,15 +340,7 @@ tap.test('connect', t => {
       t.equal(err, null);
       t.notEqual(juju, null);
       juju.login({macaroons: ['m']}, (err, conn) => {
-        helpers.requestEqual(t, ws.lastRequest, {
-          type: 'Admin',
-          request: 'Login',
-          params: {macaroons: [['m1', 'm2']]},
-          version: 3
-        });
-        t.equal(err, null);
-        t.notEqual(conn, null);
-        t.end();
+        validateLoginDischargeRequiredSuccess(t, err, conn);
       });
       // Reply to the login request with a discharge required response.
       ws.reply({response: {'discharge-required': 'macaroon'}});
@@ -237,6 +348,27 @@ tap.test('connect', t => {
       ws.reply({response: {}});
     });
     // Open the WebSocket connection.
+    ws.open();
+  });
+
+  t.test('login discharge required success via promise', t => {
+    const options = {
+      bakery: helpers.makeBakery(true),
+      wsclass: helpers.makeWSClass(instance => {
+        ws = instance;
+      })
+    };
+    jujulib.connect('wss://1.2.3.4', options).then(juju => {
+      juju.login({macaroons: ['m']})
+        .then(conn => {
+          validateLoginDischargeRequiredSuccess(t, null, conn);
+        })
+        .catch(t.fail);
+        // Reply to the login request with a discharge required response.
+        ws.reply({response: {'discharge-required': 'macaroon'}});
+        // Reply to the retried login request properly.
+        ws.reply({response: {}});
+    });
     ws.open();
   });
 
