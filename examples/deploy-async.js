2b494642b0ed3e80071e6ab618fdd3873bde993b
Jeff Pihach <jeff.pihach@canonical.com>
2018-07-31 23:16:24 -0600

    Update deploy-async example to use addCharm and deploy async.

--- examples/deploy-async.js
+++ examples/deploy-async.js
@@ -15,36 +15,26 @@ const facades = [
   require('../api/facades/client-v1.js')
 ];
 const options = {debug: true, facades: facades, wsclass: WebSocket};
-const url = 'wss://35.237.254.252:17070/model/5e764f56-c70a-4625-8556-b49c383aed69/api';
+const url = 'wss://35.227.35.32:17070/model/e713ebfd-158d-437c-80ed-c9c31e7d1bcf/api';
 
 async function connectAndDeploy() {
   try {
     const juju = await jujulib.connect(url, options);
-    const conn = await juju.login({user: 'user-admin', password: 'dfa1e626bce9f5f1d1c4b1d85768baa4'});
+    const conn = await juju.login({user: 'user-admin', password: 'secret'});
 
     const application = conn.facades.application;
     const client = conn.facades.client;
-    client.addCharm({url: 'cs:haproxy-43'}, err => {
-      if (err) {
-        console.log('cannot add charm:', err);
-        process.exit(1);
-      }
 
-      application.deploy({
-        applications: [{
-          application: 'h3',
-          charmUrl: 'cs:haproxy-43',
-          series: 'xenial'
-        }]
-      }, (err, result) => {
-        if (err) {
-          console.log('cannot deploy app:', err);
-          process.exit(1);
-        }
-        console.log(JSON.stringify(result, null, 2));
-        process.exit();
-      });
+    await client.addCharm({url: 'cs:haproxy-43'});
+    const app = await application.deploy({
+      applications: [{
+        application: 'h4',
+        charmUrl: 'cs:haproxy-43',
+        series: 'xenial'
+      }]
     });
+    console.log(JSON.stringify(app, null, 2));
+    process.exit();
   } catch(error) {
     console.log('unable to connect and deploy:', error);
     process.exit(1);
