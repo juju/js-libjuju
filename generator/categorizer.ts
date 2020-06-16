2758e8f474f296e116a966cf0465b193adf3dea4
Jeff Pihach <jeff.pihach@canonical.com>
2020-06-16 17:12:30 -0600

    remove categorizer.

--- generator/categorizer.ts
+++
@@ -1,7 +0,0 @@
-export function isInController(availableTo: string[]): boolean {
-  return availableTo.includes("controller-user");
-}
-
-export function isInModel(availableTo: string[]): boolean {
-  return availableTo.includes("model-user");
-}
