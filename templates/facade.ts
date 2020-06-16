af56ce265e89121ab607288e455176389a4be3ee
Jeff Pihach <jeff.pihach@canonical.com>
2020-06-16 16:18:31 -0600

    fix indentation.

--- templates/facade.ts
+++ templates/facade.ts
@@ -24,71 +24,71 @@ export default (f: FacadeTemplate): string => {
 
   const generateInterface = (i) => {
     return `
-  interface ${i.name} {
-${i.types.map(t => padString(`${t.name}: ${t.type};`, 4)).join('\n')}
-  }
+interface ${i.name} {
+${i.types.map(t => padString(`${t.name}: ${t.type};`, 2)).join('\n')}
+}
 `
   }
 
   return `
-  /**
-    Juju ${f.name} version ${f.version}.
-    This facade is available on:
+/**
+  Juju ${f.name} version ${f.version}.
+  This facade is available on:
 ${generateAvailableList(f.availableTo)}
 
-    NOTE: This file was generated on ${new Date(Date.now()).toUTCString()} using
-    the Juju schema from  Juju ${f.jujuVersion} at the git SHA ${f.jujuGitSHA}.
-    Do not manually edit this file.
-  */
+  NOTE: This file was generated on ${new Date(Date.now()).toUTCString()} using
+  the Juju schema from  Juju ${f.jujuVersion} at the git SHA ${f.jujuGitSHA}.
+  Do not manually edit this file.
+*/
 
-  import {autoBind, createAsyncHandler} from "../transform.js";
-  import wrappers from "../wrappers.js";
+import {autoBind, createAsyncHandler} from "../transform.js";
+import wrappers from "../wrappers.js";
 
 ${f.interfaces.map(generateInterface).join('\n')}
 
-  /**
-${padString(f.docBlock, 4)}
-  */
-  export default class ${f.name}V${f.version} {
-    version: number;
+/**
+${padString(f.docBlock, 2)}
+*/
+export default class ${f.name}V${f.version} {
+  version: number;
 
-    constructor() {
-      this.version = ${f.version};
+  constructor() {
+    this.version = ${f.version};
 
-      // Automatically bind all methods to instances.
-      autoBind(this);
-    }
-    ${f.methods.map(m =>`
-
-    /**
-${padString(m.docBlock, 6)}
-    */
-    ${lowerCaseFirstChar(m.name)}(${m.params ? `params: ${m.name}Params, callback?`: 'callback?'}) {
-      return new Promise((resolve, reject) => {
-
-        const req = {
-          type: '${f.name}',
-          request: '${m.name}',
-          version: '${f.version}',
-          params: params
-        };
-
-        let transform = null;
-        ${m.result ? `
-        transform = resp => {
-          let result;
-          // XXX generate the result
-          return result;
-        };
-        `: ``}
-        const handler = createAsyncHandler(callback, resolve, reject, transform);
-        this._transport.write(req, handler);
-      });
-    }
-    `).join('')}
-    if (wrappers.wrap${f.name}) {
-      ${f.name}V${f.version} = wrappers.wrap${f.name}(${f.name}V${f.version});
-    }
+    // Automatically bind all methods to instances.
+    autoBind(this);
   }
-  `;
+  ${f.methods.map(m =>`
+
+  /**
+${padString(m.docBlock, 4)}
+  */
+  ${lowerCaseFirstChar(m.name)}(${m.params ? `params: ${m.name}Params, callback?`: 'callback?'}) {
+    return new Promise((resolve, reject) => {
+
+      const req = {
+        type: '${f.name}',
+        request: '${m.name}',
+        version: '${f.version}',
+        params: params
+      };
+
+      let transform = null;
+      ${m.result ? `
+      transform = resp => {
+        let result;
+        // XXX generate the result
+        return result;
+      };
+      `: ``}
+      const handler = createAsyncHandler(callback, resolve, reject, transform);
+      this._transport.write(req, handler);
+    });
+  }
+  `).join('')}
+  if (wrappers.wrap${f.name}) {
+    ${f.name}V${f.version} = wrappers.wrap${f.name}(${f.name}V${f.version});
+  }
+}
+`;
 }
