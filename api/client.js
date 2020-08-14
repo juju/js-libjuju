64c91003f39c37df2698d59084b551ec2526e105
Jeff Pihach <jeff.pihach@canonical.com>
2020-08-14 17:18:49 -0600

    Update to the new api format.

--- api/client.js
+++ api/client.js
@@ -3,45 +3,15 @@
 
 /**
   This library is the JavaScript Juju API client. Use the connect function to
-  get access to the API. Provide your required facade classes in options, like:
-
-    const facades = [
-      require('jujulib/api/facades/all-watcher-v1'),
-      require('jujulib/api/facades/application-v5'),
-      require('jujulib/api/facades/client-v1'),
-      require('jujulib/api/facades/client-v2'),
-      require('jujulib/api/facades/pinger-v1')
-    ];
-    jujulib.connect('wss://example.com', {facades: facades}, (err, juju) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      juju.login({user: 'user-admin', password: 'aaa'}, (err, conn) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        const client = conn.facades.client;
-        const handle = client.watch((err, result) => {
-          if (err) {
-            console.log('cannot watch model:', err);
-            return;
-          }
-          console.log(result);
-        });
-        setTimeout(handle.stop, 5000);
-      });
-    });
+  get access to the API. See the examples/ folder for detailed examples on how
+  to use this API.
 */
 
-'use strict';
+"use strict";
 
+import Admin from "./facades/admin-v3.js";
 
-const Admin = require('./facades/admin-v3.js');
-
-const {createAsyncHandler} = require('./transform');
-
+import { createAsyncHandler } from "./transform.js";
 
 /**
   Connect to the Juju controller or model at the given URL.
@@ -72,7 +42,7 @@ const {createAsyncHandler} = require('./transform');
     connecting, or resolved with a new Client instance. Note that the promise
     will not be resolved or rejected if a callback is provided.
 */
-function connect(url, options={}, callback=null) {
+function connect(url, options = {}, callback = null) {
   if (!options.bakery) {
     options.bakery = null;
   }
@@ -86,22 +56,24 @@ function connect(url, options={}, callback=null) {
     options.facades = [];
   }
   if (!options.wsclass) {
-    options.wsclass = window.WebSocket;
+    console.error("No websocket provided. define 'wsclass'.");
   }
   return new Promise((resolve, reject) => {
     // Instantiate the WebSocket, and make the client available when the
     // connection is open.
     const ws = new options.wsclass(url);
     const handler = createAsyncHandler(callback, resolve, reject);
-    ws.onopen = evt => {
+    ws.onopen = (evt) => {
       handler(null, new Client(ws, options));
     };
-    ws.onclose = evt => {
-      handler('cannot connect WebSocket: ' + evt.reason, null);
+    ws.onclose = (evt) => {
+      handler("cannot connect WebSocket: " + evt.reason, null);
+    };
+    ws.onerror = (evt) => {
+      console.log("--", evt);
     };
   });
 }
-
 
 /**
   Connect to the Juju controller or model at the given URL and the authenticate
@@ -130,57 +102,38 @@ function connect(url, options={}, callback=null) {
       see <https://www.npmjs.com/package/macaroon-bakery>;
     - closeCallback: a callback to be called with the exit code when the
       connection is closed.
-  @param {Function} callback Called when the login process completes, the
-    callback receives an error, or a {conn, logout} result including a
-    connection object and a logout function that can be used to close the
-    connection. If there are no errors, the connection can be used to
-    send/receive messages to and from the Juju controller or model, and to get
-    access to the available facades (through conn.facades). See the docstring
-    for the Connection class for information on how to use the connection
-    instance. Redirection errors are automatically handled by this function, so
-    any error is a real connection problem.
   @return {Promise} This promise will be rejected if there is an error
     connecting, or resolved with a new {conn, logout} object. Note that the
     promise will not be resolved or rejected if a callback is provided.
 */
-function connectAndLogin(url, credentials, options, callback) {
-  return new Promise((resolve, reject) => {
-    const handler = createAsyncHandler(callback, resolve, reject);
+function connectAndLogin(url, credentials, options) {
+  return new Promise(async (resolve, reject) => {
     // Connect to Juju.
-    connect(url, options, (err, juju) => {
-      if (err) {
-        handler(err, null);
-        return;
+    let juju;
+    try {
+      juju = await connect(url, options);
+      const conn = await juju.login(credentials);
+      resolve({ conn, logout: juju.logout.bind(juju) });
+    } catch (error) {
+      if (!juju.isRedirectionError(error)) {
+        reject(error);
       }
-      // Authenticate.
-      juju.login(credentials, (err, conn) => {
-        if (!err) {
-          handler(null, {conn: conn, logout: juju.logout.bind(juju)});
-          return;
+      // Redirect to the real model.
+      juju.logout();
+      for (let i = 0; i < error.servers.length; i++) {
+        const srv = error.servers[i];
+        // TODO(frankban): we should really try to connect to all servers and
+        // just use the first connection available, without second guessing
+        // that the public hostname is reachable.
+        if (srv.type === "hostname" && srv.scope === "public") {
+          // This is a public server with a dns-name, connect to it.
+          resolve(connectAndLogin(srv.url(url), credentials, options));
         }
-        if (!juju.isRedirectionError(err)) {
-          handler(err, null);
-          return;
-        }
-        // Redirect to the real model.
-        juju.logout();
-        for (let i = 0; i < err.servers.length; i++) {
-          const srv = err.servers[i];
-          // TODO(frankban): we should really try to connect to all servers and
-          // just use the first connection available, without second guessing
-          // that the public hostname is reachable.
-          if (srv.type === 'hostname' && srv.scope === 'public') {
-            // This is a public server with a dns-name, connect to it.
-            connectAndLogin(srv.url(url), credentials, options, handler);
-            return;
-          }
-        }
-        handler(new Error('cannot connect to model after redirection'), null);
-      });
-    });
+      }
+      reject("cannot connect to model after redirection");
+    }
   });
 }
-
 
 /**
   Returns a URL that is to be used to connect to a supplied model uuid on the
@@ -194,7 +147,6 @@ function generateModelURL(controllerHost, modelUUID) {
   return `wss://${controllerHost}/model/${modelUUID}/api`;
 }
 
-
 /**
   A Juju API client allowing for logging in and get access to facades.
 
@@ -204,7 +156,6 @@ function generateModelURL(controllerHost, modelUUID) {
     above for a description of available options.
 */
 class Client {
-
   constructor(ws, options) {
     // Instantiate the transport, used for sending messages to the server.
     this._transport = new Transport(ws, options.closeCallback, options.debug);
@@ -221,70 +172,63 @@ class Client {
       If an empty object is provided a full bakery discharge will be attempted
       for logging in with macaroons. Any necessary third party discharges are
       performed using the bakery instance originally provided to connect().
-    @param {Function} [callback=null] Called when the login process completes,
-      the callback receives an error and a connection object. If there are no
-      errors, the connection can be used to send/receive messages to and from
-      the Juju controller or model, and to get access to the available facades
-      (through conn.facades). See the docstring for the Connection class for
-      information on how to use the connection instance. If an error is
-      returned, clients can check whether it's a redirection error as done by
-      JAAS when a new connection to the actual remote model is required. You
-      can check if that's the case using client.isRedirectionError(err). If it
-      is a redirection error, information about available servers is stored in
-      err.servers and err.caCert (if a certificate is required).
     @return {Promise} This promise will be rejected if there is an error
       connecting, or resolved with a new connection instance. Note that the
       promise will not be resolved or rejected if a callback is provided.
   */
-  login(credentials, callback=null) {
-    const args = {
-      authTag: credentials.user,
-      credentials: credentials.password,
-      macaroons: credentials.macaroons
-    };
-    return new Promise((resolve, reject) => {
-      const handler = createAsyncHandler(callback, resolve, reject);
-      this._admin.login(args, (err, result) => {
-        if (err) {
-          if (err !== REDIRECTION_ERROR) {
-            // Authentication failed.
-            handler(err, null);
-            return;
-          }
-          // This is a model redirection error, so retrieve some info now.
-          this._admin.redirectInfo((err, result) => {
-            if (err) {
-              handler(err, null);
-              return;
-            }
-            handler(new RedirectionError(result.servers, result.caCert), null);
-          });
-          return;
-        }
-        // Handle bakery discharge required responses.
-        if (result.dischargeRequired) {
+  login(credentials) {
+    const args = {};
+    if (credentials.password) {
+      args.credentials = credentials.password;
+    }
+    if (credentials.macaroons) {
+      args.macaroons = credentials.macaroons;
+    }
+    if (credentials.username) {
+      args["auth-tag"] = `user-${credentials.username}`;
+    }
+
+    return new Promise(async (resolve, reject) => {
+      try {
+        const response = await this._admin.login(args);
+        if (response["discharge-required"]) {
           if (!this._bakery) {
-            handler(
-              'macaroon discharge is required but no bakery instance provided',
-              null);
+            reject(
+              "macaroon discharge required but no bakery instance provided"
+            );
             return;
           }
-          const onSuccess = macaroons => {
+          const onSuccess = (macaroons) => {
             // Send the login request again including the discharge macaroons.
             credentials.macaroons = [macaroons];
-            this.login(credentials, handler);
+            return resolve(this.login(credentials));
           };
-          const onFailure = err => {
-            handler('macaroon discharge failed: ' + err, null);
+          const onFailure = (err) => {
+            reject("macaroon discharge failed: " + err);
           };
-          this._bakery.discharge(result.dischargeRequired, onSuccess, onFailure);
+          this._bakery.discharge(
+            response["discharge-required"],
+            onSuccess,
+            onFailure
+          );
           return;
         }
-
-        // Authentication succeeded.
-        const conn = new Connection(this._transport, this._facades, result);
-        handler(null, conn);
-      });
+        resolve(new Connection(this._transport, this._facades, response));
+      } catch (error) {
+        if (error !== REDIRECTION_ERROR) {
+          reject(error);
+          return;
+        }
+        // This is a model redirection error, fetch the redirection information.
+        try {
+          const info = await this._admin.redirectInfo();
+          reject(new RedirectionError(info));
+          return;
+        } catch (error) {
+          reject(error);
+          return;
+        }
+      }
     });
   }
 
@@ -309,16 +253,14 @@ class Client {
   isRedirectionError(err) {
     return err instanceof RedirectionError;
   }
-
 }
 
-
 // Define the redirect error returned by Juju, and the one returned by the API.
-const REDIRECTION_ERROR = 'redirection required';
+const REDIRECTION_ERROR = "redirection required";
 class RedirectionError {
-  constructor(servers, caCert) {
-    this.servers = servers;
-    this.caCert = caCert;
+  constructor(info) {
+    this.servers = info.servers;
+    this.caCert = info["ca-cert"];
   }
 }
 
@@ -334,22 +276,21 @@ class RedirectionError {
     level.
 */
 class Transport {
-
   constructor(ws, closeCallback, debug) {
     this._ws = ws;
     this._counter = 0;
     this._callbacks = {};
     this._closeCallback = closeCallback;
     this._debug = debug;
-    ws.onmessage = evt => {
+    ws.onmessage = (evt) => {
       if (this._debug) {
-        console.debug('<--', evt.data);
+        console.debug("<--", evt.data);
       }
       this._handle(evt.data);
     };
-    ws.onclose = evt => {
+    ws.onclose = (evt) => {
       if (this._debug) {
-        console.debug('close:', evt.code, evt.reason);
+        console.debug("close:", evt.code, evt.reason);
       }
       this._closeCallback(evt.code);
     };
@@ -375,11 +316,11 @@ class Transport {
     }
     this._counter += 1;
     // Include the current request id in the request.
-    req['request-id'] = this._counter;
+    req["request-id"] = this._counter;
     this._callbacks[this._counter] = callback;
     const msg = JSON.stringify(req);
     if (this._debug) {
-      console.debug('-->', msg);
+      console.debug("-->", msg);
     }
     // Send the request to Juju.
     this._ws.send(msg);
@@ -394,7 +335,7 @@ class Transport {
   */
   close(callback) {
     const closeCallback = this._closeCallback;
-    this._closeCallback = code => {
+    this._closeCallback = (code) => {
       if (callback) {
         callback(code, closeCallback);
         return;
@@ -412,16 +353,14 @@ class Transport {
   */
   _handle(data) {
     const resp = JSON.parse(data);
-    const id = resp['request-id'];
+    const id = resp["request-id"];
     const callback = this._callbacks[id];
     delete this._callbacks[id];
     if (callback) {
       callback.call(this, resp.error || null, resp.response || {});
     }
   }
-
 }
-
 
 /**
   A connection to a Juju controller or model. This is the object users use to
@@ -443,7 +382,6 @@ class Transport {
     available via the info property of the connection instance.
 */
 class Connection {
-
   constructor(transport, facades, loginResult) {
     // Store the transport used for sending messages to Juju.
     this.transport = transport;
@@ -456,32 +394,27 @@ class Connection {
       serverVersion: loginResult.serverVersion,
       servers: loginResult.servers,
       user: loginResult.userInfo,
-      getFacade: name => {
+      getFacade: (name) => {
         return this.facades[name];
-      }
+      },
     };
 
     // Handle facades.
     const registered = facades.reduce((previous, current) => {
-      previous[current.name] = current;
+      previous[current.NAME] = current;
       return previous;
     }, {});
     this.facades = loginResult.facades.reduce((previous, current) => {
-      for (let i = current.versions.length-1; i >= 0; i--) {
-        const className = current.name + 'V' + current.versions[i];
-        const facadeClass = registered[className];
-        if (facadeClass) {
-          const facadeName = uncapitalize(current.name);
-          previous[facadeName] = new facadeClass(this.transport, this.info);
-          return previous;
-        }
+      const facadeClass = registered[current.name];
+      if (facadeClass && current.versions.includes(facadeClass.VERSION)) {
+        const facadeName = uncapitalize(facadeClass.NAME);
+        previous[facadeName] = new facadeClass(this.transport, this.info);
+        return previous;
       }
       return previous;
     }, {});
   }
-
 }
-
 
 /**
   Convert ThisString to thisString and THATString to thatString.
@@ -491,9 +424,9 @@ class Connection {
 */
 function uncapitalize(text) {
   if (!text) {
-    return '';
+    return "";
   }
-  const isLower = char => char.toLowerCase() === char;
+  const isLower = (char) => char.toLowerCase() === char;
   if (isLower(text[0])) {
     return text;
   }
@@ -508,8 +441,8 @@ function uncapitalize(text) {
   if (uppers.length > 1) {
     uppers.pop();
   }
-  const prefix = uppers.join('');
+  const prefix = uppers.join("");
   return prefix.toLowerCase() + text.slice(prefix.length);
 }
 
-module.exports = {Client, connect, connectAndLogin, generateModelURL};
+export { Client, connect, connectAndLogin, generateModelURL };
