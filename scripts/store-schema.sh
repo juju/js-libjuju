b3ac0f2babecc32a7194decc2c9e2fe3c4309be7
Pen Gale <petevg@canonical.com>
2022-07-05 13:15:16 -0400

    Update to support Juju 3.0
    Tweaked store-schema script so that it was a bit friendly to the
    current best practices around cloning and building Juju.

--- scripts/store-schema.sh
+++ scripts/store-schema.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-SCHEMA="`echo $GOPATH`/src/github.com/juju/juju/apiserver/facades/schema.json"
-cd $GOPATH/src/github.com/juju/juju
+if [ -z "$JUJU_SRC" ]; then
+    # Go is moving away from GOPATH. It's better to override this with `export JUJU_SRC=/path/to/juju`
+    JUJU_SRC=$GOPATH/src/github.com/juju/juju/
+fi
+SCHEMA="$JUJU_SRC/apiserver/facades/schema.json"
+cd $JUJU_SRC
 GIT_SHA=`git rev-parse --short HEAD`
-JUJU_VERSION=`cat $GOPATH/src/github.com/juju/juju/version/version.go | grep -P '(const version = )"(.*)"' -o | grep -P '"(.*)"' -o | tr -d '"'`
+JUJU_VERSION=`cat $JUJU_SRC/version/version.go | grep -P '(const version = )"(.*)"' -o | grep -P '"(.*)"' -o | tr -d '"'`
 cd -
 echo -n $JUJU_VERSION > schema/juju-version.txt
 echo -n $GIT_SHA > schema/juju-git-sha.txt
